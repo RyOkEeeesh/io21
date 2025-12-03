a878b0b3eef57a3900aa7de53114c5024acde1dc
RyOkEeeesh <dafe00842202@outlook.jp>
2025-12-03 17:06:01 +0900

    update
    `npm run build`からお願い

--- kadai06/next/app/api/data/route.ts
+++ kadai06/next/app/api/data/route.ts
@@ -3,53 +3,49 @@ import * as fs from 'fs/promises';
 import path from 'path';
 import { Data, DataKey } from '@/types/data';
 
-const CSV_PATH = path.join(process.cwd(), 'data', 'kadai06.csv');;
-// const CSV_PATH = path.join('/data', 'kadai06.csv');
+const CSV_PATH = path.join('/data', 'kadai06.csv');
 
 export async function GET() {
   try {
     const fileContent = await fs.readFile(CSV_PATH, 'utf-8');
-
     const lines = fileContent.split('\n').filter(line => line.trim() !== '');
 
-    if (lines.length <= 1) {
-      return NextResponse.json({ data: [], error: "CSVファイルにデータが見つかりません。" }, { status: 200 });
-    }
+    if (lines.length <= 1)
+      return NextResponse.json({ data: [] }, { status: 200 });
 
     const data: Data = {
       time: [],
       temperature: [],
       humidity: [],
     };
-    const headers: DataKey[] = lines[0].split(',').map(h => h.trim()) as DataKey[];;
+    const headers: DataKey[] = lines[0]
+      .split(',')
+      .map(h => h.trim()) as DataKey[];
 
-    lines.slice(1).slice(-60).forEach(line => {
-      const values = line.split(',');
+    lines
+      .slice(1)
+      .slice(-60)
+      .forEach(line => {
+        const values = line.split(',');
 
-      headers.forEach((header, index) => {
-        const valueString = values[index]?.trim() ?? '';
+        headers.forEach((header, index) => {
+          const valueString = values[index]?.trim() ?? '';
 
-        if (header === 'time') {
-          data.time.push(valueString);
-          return
-        }
-        const valNam = Number(valueString);
-        data[header].push(isNaN(valNam) ? null : valNam);
+          if (header === 'time') {
+            data.time.push(valueString);
+            return;
+          }
+          const valNam = Number(valueString);
+          data[header].push(isNaN(valNam) ? null : valNam);
+        });
       });
-    });
 
     return NextResponse.json({ data }, { status: 200 });
-
   } catch (error: any) {
-    if (error.code === 'ENOENT') {
-      return NextResponse.json({
-        error: 'Data file not found or Python not started yet.'
-      }, { status: 404 });
-    }
+    if (error.code === 'ENOENT')
+      return NextResponse.json({ status: 404 });
 
-    console.error("Failed to read or parse CSV:", error);
-    return NextResponse.json({
-      error: `Internal Server Error during file reading/parsing: ${error.message}`
-    }, { status: 500 });
+    console.error('Failed to read or parse CSV:', error);
+    return NextResponse.json({ status: 500 });
   }
 }
--- kadai06/next/app/page.tsx
+++ kadai06/next/app/page.tsx
@@ -1,6 +1,6 @@
-"use client";
-import { useEffect, useRef, useState } from "react";
-import { Data } from "@/types/data";
+'use client';
+import { useEffect, useRef, useState } from 'react';
+import { Data } from '@/types/data';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,7 +11,7 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
-import { Line } from "react-chartjs-2";
+import { Line } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
@@ -24,21 +24,35 @@ ChartJS.register(
 );
 
 function Graph({ datas }: { datas: Data }) {
+  const [isDark, setIsDark] = useState<boolean>(null!);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    setIsDark(mediaQuery.matches);
+
+    function handleChange(e: MediaQueryListEvent) {
+      setIsDark(e.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, []);
+
   const data = {
     labels: datas.time,
     datasets: [
       {
         label: '温度',
         data: datas.temperature,
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        
+        borderColor: 'rgb(255, 32, 86, 0.5)',
+        backgroundColor: 'rgb(255, 99, 132)',
+        yAxisID: 'yl' as const,
       }, {
         label: '湿度',
         data: datas.humidity,
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-        yAxisID: 'yRight' as const,
+        borderColor: 'rgba(21, 93, 252, 0.5)',
+        backgroundColor: 'rgb(53, 162, 235)',
+        yAxisID: 'yr' as const,
       }
     ]
   }
@@ -52,31 +66,49 @@ function Graph({ datas }: { datas: Data }) {
       },
       title: {
         display: true,
-        text: '温度・湿度',
+        text: 'IO21 課題6',
+        font: {
+          size: 32,
+          weight: 'bold' as const,
+        },
       },
     },
     scales: {
-      y: {
+      x: {
+        grid: {
+          color: isDark ? '#292524' : '#d6d3d1',
+        }
+      },
+      yl: {
         type: 'linear' as const,
         display: true,
         position: 'left' as const,
         title: {
           display: true,
           text: '温度 (°C)',
+          color: 'rgb(255, 99, 132)',
+          font: {
+            size: 20,
+          }
         },
         grid: {
           drawOnChartArea: true,
+          color: isDark ? '#292524' : '#d6d3d1',
         },
-        min: 0,
-        max: 40,
+        min: 10,
+        max: 30,
       },
-      yRight: {
+      yr: {
         type: 'linear' as const,
         display: true,
         position: 'right' as const,
         title: {
           display: true,
           text: '湿度 (%)',
+          color: 'rgb(53, 162, 235)',
+          font: {
+            size: 20,
+          }
         },
         grid: {
           drawOnChartArea: false,
@@ -88,7 +120,9 @@ function Graph({ datas }: { datas: Data }) {
   };
 
   return (
-    <Line className="w-full" data={data} options={options} />
+    <div className="h-[70vmin] aspect-video">
+      <Line data={data} options={options} />
+    </div>
   )
 }
 
@@ -112,35 +146,23 @@ export default function App() {
 
   const wsRef = useRef<WebSocket>(null!);
 
-  // useEffect(() => {
-  //   try {
-  //     wsRef.current = new WebSocket(`ws://${(window as Window).location.hostname}:3001`);
-  //     console.log('wsに接続しました');
-  //   } catch {
-  //     console.error('wsに接続できません');
-  //     return;
-  //   }
-  //   wsRef.current.onopen = () => { dataFetch(); }
-  //   wsRef.current.onmessage = msg => {
-  //     const data = JSON.parse(msg.data);
-  //     if (data.type === 'data_updated') {
-  //       console.log('csv update');
-  //       dataFetch();
-  //     }
-  //   };
-  //   return () => { wsRef.current.close(); }
-  // }, []);
-
   useEffect(() => {
-    dataFetch();
+    try {
+      wsRef.current = new WebSocket(`ws://${(window as Window).location.hostname}:3001`);
+    } catch {
+      return;
+    }
+    wsRef.current.onopen = () => { dataFetch(); }
+    wsRef.current.onmessage = msg => {
+      const data = JSON.parse(msg.data);
+      if (data.type === 'data_updated') dataFetch();
+    };
+    return () => { wsRef.current.close(); }
   }, []);
 
-  console.log(datas)
-
   return (
-    <div className="h-full w-full flex items-center justify-center">
+    <div className='h-full w-full flex items-center justify-center'>
       <Graph datas={datas} />
     </div>
-
   );
 }
